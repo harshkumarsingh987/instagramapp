b443532c4fc5ce12ab2460332c617012b63e7cf8
harshkumarsingh <143312954+harshkumarsingh987@users.noreply.github.com>
2026-02-12 11:55:47 +0530

    Update login API endpoint to production URL

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -35,7 +35,7 @@ function Login() {
 
     try {
     const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://instagram-backend.onrender.com/api/auth/login",
         { email, password },
         { withCredentials: true } // allows cookies
       );
